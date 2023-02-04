ef20845825304ec5263e5a5224e0f6c6f5d355e9
Itz OP <106766934+ItzOP69@users.noreply.github.com>
2023-02-04 09:42:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,13 +1,17 @@
-#Author:- Dynamax
+#Author:- SwastikPlay
 
-x = int(input("Enter the number:- "))
-n = int(input("Enter the number of terms:- "))
-i = ((x+n)**2)/(x**2 - n**2)
-S = 0
-print("S =", end=" ")
-for j in range(0, n):
-    S += (((( n+i/x+i+j )*( n+j+i/x**(j + 2) )) **(j + 2) ) ) / i
-    if j != n-1:
-        print(f'(((( {n}+{i}/{x}+{i}+{j} )*( {n}+{j}+{i}/{x}^({j} + 2) )) ^ ({j} + 2) ) ) / {i} + ', end=" ")
-    else:
-        print(f'(((( {n}+{i}/{x}+{i}+{j} )*( {n}+{j}+{i}/{x}^({j} + 2) )) ^ ({j} + 2) ) ) / {i} = {S}', end=" ")
+try:
+    x = int(input("Enter the number:- "))
+    n = int(input("Enter the number of terms:- "))
+    i = ((x+n)**2)/(x**2 - n**2)
+    S = 0
+    print("S =", end=" ")
+    for j in range(0, n):
+        S += (((( n+i/x+i+j )*( n+j+i/x**(j + 2) )) **(j + 2) ) ) / i
+        if j != n-1:
+            print(f'(((( {n}+{i}/{x}+{i}+{j} )*( {n}+{j}+{i}/{x}^({j} + 2) )) ^ ({j} + 2) ) ) / {i} + ', end=" ")
+        else:
+            print(f'(((( {n}+{i}/{x}+{i}+{j} )*( {n}+{j}+{i}/{x}^({j} + 2) )) ^ ({j} + 2) ) ) / {i} = {S}', end=" ")
+except:
+    print(" ")
+    print("Ran into an error.")
